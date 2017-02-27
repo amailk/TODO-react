c33bdfe640d260db9dfbe1c18d524deac3944ab3
Ama Liyanage <liyaniama@gmail.com>
2017-02-27 15:00:12 -0500

    todoitem.js

--- src/components/todo/TodoList.js
+++ src/components/todo/TodoList.js
@@ -1,7 +1,13 @@
 import React from 'react'
+import {TodoItem} from './TodoItem'
 
 export const TodoList = (props) => {
   return (
-    <div> Todo List </div>
+    <div className="Todo-List">
+      <ul>
+        {props.todos.map(todo =>
+          <TodoItem id={todo.id} name={todo.name}/>)}
+      </ul>
+    </div>
   )
 }
