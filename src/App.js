ed50ada7d3d675298456cf6aeb754a31e34cbf13
Ama Liyanage <liyaniama@gmail.com>
2017-02-27 14:48:37 -0500

    todoform.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {TodoForm} from './components/todo/TodoForm'
 
 class App extends Component {
   constructor() {
@@ -10,8 +11,16 @@ class App extends Component {
       {id: 1, name: 'Learn JSX', isComplete: true},
       {id: 2, name: 'Build a cool App', isComplete: false},
       {id: 3, name: 'Ship it!', isComplete: false}
-    ]
+    ],
+    currentTodo: ''
     }
+    this.handleInputChange = this.handleInputChange.bind(this)
+  }
+
+  handleInputChange (evt) {
+    this.setState({
+      currentTodo: evt.target.value
+    })
   }
   render() {
     return (
@@ -21,9 +30,8 @@ class App extends Component {
           <h2>React Todos</h2>
         </div>
         <div className="Todo-App">
-          <form>
-            <input type="text"/>
-          </form>
+          <TodoForm handleInputChange={this.handleInputChange}
+          currentTodo={this.state.currentTodo}/>
         </div>
         <div className="Todo-List">
           <ul>
