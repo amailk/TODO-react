89068aea2c33eef5874f27ba5c4a9847cbc7a27d
Ama Liyanage <liyaniama@gmail.com>
2017-02-27 18:20:43 -0500

    toggle todos

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,1 +1,5 @@
 export const partial = (fn, ...args) => fn.bind(null, ...args) //comma seperated arguments bundled in to an array(1), second one is a spread operator(spread as arguments)
+
+const _pipe = (f, g) => (...args) => g(f(...args))
+
+export const pipe = (...fns) => fns.reduce(_pipe)
