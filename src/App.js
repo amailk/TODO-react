06cd6f163d69a0017ab15b3b70dfc87f1c2d1451
Ama Liyanage <liyaniama@gmail.com>
2017-02-27 13:12:21 -0500

    constructor and default checked.

--- src/App.js
+++ src/App.js
@@ -3,16 +3,37 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      todos: [
+      {id: 1, name: 'Learn JSX', isComplete: true},
+      {id: 2, name: 'Build a cool App', isComplete: false},
+      {id: 3, name: 'Ship it!', isComplete: false}
+    ]
+    }
+  }
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>React Todos</h2>
         </div>
-        <p className="App-intro">
-          Hello, React!
-        </p>
+        <div className="Todo-App">
+          <form>
+            <input type="text"/>
+          </form>
+        </div>
+        <div className="Todo-List">
+          <ul>
+            {this.state.todos.map(todo =>
+              <li key={todo.id}>
+                <input type="checkbox" defaultChecked={todo.isComplete}/> {todo.name}
+              </li>)}
+          </ul>
+        </div>
+
       </div>
     );
   }
