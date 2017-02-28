b9c142c25c5ded94d875c3d00e94ba2267f7a1e6
Ama Liyanage <liyaniama@gmail.com>
2017-02-28 11:30:52 -0500

    db.json , adding a new todo item

--- src/App.js
+++ src/App.js
@@ -4,14 +4,11 @@ import './App.css';
 import {TodoForm, TodoList, Footer} from './components/todo'
 import {addTodo, generateId, findById, toggleTodo, updateTodo, removeTodo, filterTodos} from './lib/todoHelpers'
 import {pipe, partial} from './lib/utils'
+import {loadTodos, createTodo} from './lib/todoService'
 
 class App extends Component {
   state = {
-    todos: [
-    {id: 1, name: 'Learn JSX', isComplete: true},
-    {id: 2, name: 'Build a cool App', isComplete: false},
-    {id: 3, name: 'Ship it!', isComplete: false}
-  ],
+    todos: [],
   currentTodo: ''
   }
 
@@ -19,6 +16,10 @@ class App extends Component {
     route: React.PropTypes.string
   }
 
+  componentDidMount() {
+    loadTodos()
+      .then(todos => this.setState({todos}))
+  }
   handleRemove = (id, evt) => {
     evt.preventDefault()
     const updatedTodos = removeTodo(this.state.todos, id)
@@ -44,6 +45,8 @@ class App extends Component {
       currentTodo: '',
       errorMessage: ''
     })
+    createTodo(newTodo)
+      .then(() => console.log('Todo added'))
   }
 
   handleEmptySubmit = (evt) => {
