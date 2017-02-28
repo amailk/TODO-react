a8a4c18e0b0c3845966f1237091d484fed556820
Ama Liyanage <liyaniama@gmail.com>
2017-02-28 10:55:59 -0500

    linkHandler

--- src/components/router/Link.js
+++ src/components/router/Link.js
@@ -1,7 +1,21 @@
 import React, {Component} from 'react'
 
 export class Link extends Component {
-  render() {
-    return <a href="this.props.to">{this.props.children}</a>
+  static contextTypes = {
+    route: React.PropTypes.string,
+    linkHandler: React.PropTypes.func
   }
+
+  handleClick = (evt) => {
+    evt.preventDefault()
+    history.pushState(null, '', this.props.to)
+  }
+
+  render() {
+    return <a href="#" onClick={this.handleClick}>{this.props.children}</a>
+  }
+}
+
+Link.propTyes = {
+  to: React.PropTypes.string.isRequired
 }
